2d43d86d379016bdb70442984b4e7584f2a92f5d
LuchoDelArco <47138072@est.ort.edu.ar>
2022-04-03 16:53:22 -0300

    Password Validation Completed

--- Assets/Scenes/Scripts/ValidarContrasenia.cs
+++ Assets/Scenes/Scripts/ValidarContrasenia.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ValidarContrasenia : MonoBehaviour
 {
+	string contraseniaCorrecta;
+	public Text ingresoUsuario;
+	string contraseniaUsuario;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+		contraseniaCorrecta = "12345";
     }
 
     // Update is called once per frame
@@ -18,6 +24,14 @@ public class ValidarContrasenia : MonoBehaviour
 
 	public void validarContrasenia()
 	{
-
+		contraseniaUsuario = ingresoUsuario.text;
+		if (contraseniaUsuario == contraseniaCorrecta)
+		{
+			Debug.Log("Bienvenido");
+		}
+		else
+		{
+			Debug.Log("Contraseña incorrecta");
+		}
 	}
 }
